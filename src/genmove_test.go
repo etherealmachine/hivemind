8093c7be9aa8e6cf9e0d66a82704100b2bbb7675
James Pettit <etherealmachine@gmail.com>
2011-08-07 11:07:27 -0700

    Added swarm tests to test suite. Requires a trained swarm to run.

--- src/genmove_test.go
+++ src/genmove_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "fmt"
 
 var config *Config
 
@@ -12,6 +13,7 @@ func setupConfig() {
 }
 
 func TestGoGenmove(t *testing.T) {
+	fmt.Println("Go Genmove")
 	setupConfig()
 	config.Go = true
 	config.MaxPlayouts = 10000
@@ -21,9 +23,32 @@ func TestGoGenmove(t *testing.T) {
 }
 
 func TestHexGenmove(t *testing.T) {
+	fmt.Println("Hex Genmove")
 	setupConfig()
 	config.Hex = true
 	config.MaxPlayouts = 10000
+	tracker := NewTracker(config)
+	root := NewRoot(BLACK, tracker, config)
+	genmove(root, tracker)
+}
+
+func TestGoSwarm(t *testing.T) {
+	fmt.Println("Go Swarm")
+	setupConfig()
+	config.Go = true
+	config.MaxPlayouts = 10000
+        config.policy_weights = LoadBest("./swarm.1.gob", config)
+	tracker := NewTracker(config)
+	root := NewRoot(BLACK, tracker, config)
+	genmove(root, tracker)
+}
+
+func TestHexSwarm(t *testing.T) {
+	fmt.Println("Hex Swarm")
+	setupConfig()
+	config.Hex = true
+	config.MaxPlayouts = 10000
+        config.policy_weights = LoadBest("./swarm.1.gob", config)
 	tracker := NewTracker(config)
 	root := NewRoot(BLACK, tracker, config)
 	genmove(root, tracker)
